ecb0d1d1b11f23cc97f96bbec3ac090e5b2c76f4
winnapat <GGgun2013@gmail.com>
2021-07-25 18:45:46 +0700

    add reset button add list done list

--- script.js
+++ script.js
@@ -1,42 +1,77 @@
-
-/*add and delete todo list*/
-let currentInput = ''
-
 //input task
 const input = document.querySelector('input')
-input.addEventListener( 'input', ev => {
-	currentInput = ev.target.value
-})
+input.placeholder = "Enter list here"
 
 //list (ul, li)
-const mainDiv = document.createElement('div')
+const undoneDiv = document.querySelector('#undonediv')
+const doneDiv = document.querySelector('#donediv')
 
 //add task button
-const btn = document.querySelector('button')
-btn.addEventListener('click', () => {
-    if(currentInput !== ''){
-	const spanTask = document.createElement('span')
-	const span = document.createElement('span')
-	span.innerHTML = currentInput
-    spanTask.style.fontFamily = "Lobster, cursive"
-	spanTask.append(span)
-
-	const delBtn = document.createElement('button')
-	delBtn.innerHTML = 'delete'
-	delBtn.addEventListener('click', () => {
-		mainDiv.removeChild(spanTask)
-	})
-
-	spanTask.append(delBtn)
-	spanTask.append(document.createElement('br'))
-	mainDiv.append(spanTask)
+const Addbtn = document.querySelector('#Add')
+Addbtn.addEventListener('click', () => {
+    if(input.value == ''){
+        window.alert("Task cannot be empty na i sus")
     }
     else{
-        window.alert("Task cannot be empty")
+        const divTask = document.createElement('div')
+        divTask.setAttribute("class","flex justify-between text-xl border-b-2 border-yellow")
+        const div = document.createElement('div')
+        div.innerHTML = input.value
+        // div.style.fontFamily = "Righteous, cursive"
+        div.style.fontFamily = "Lobster, cursive"
+        divTask.append(div)
+
+        //button flex
+        const Btndiv = document.createElement('div')
+        Btndiv.setAttribute("class","flex justify-between space-x-5")
+        Btndiv.style.fontFamily = "Lobster, cursive"
+        Btndiv.style.visibility = "hidden"
+        divTask.addEventListener("mouseenter", ()=>{
+            Btndiv.style.visibility = "visible"
+        })
+        divTask.addEventListener("mouseleave", ()=>{
+            Btndiv.style.visibility = "hidden"
+        })
+
+        
+        //deleteButton
+        const delBtn = document.createElement('button')
+        delBtn.innerHTML = 'delete'
+        delBtn.setAttribute("class","bg-red-500 px-1 rounded-xl")
+        delBtn.addEventListener('click', () => {
+        undoneDiv.removeChild(divTask)
+
+        })
+
+        //doneButton
+        const doneBtn = document.createElement('button')
+        doneBtn.innerHTML = 'done'
+        doneBtn.setAttribute("class","bg-yellow-500 px-1 rounded-xl")
+        doneBtn.addEventListener('click', () => {
+        doneDiv.insertBefore(divTask,doneDiv.firstChild)
+        div.style.textDecoration = 'line-through'
+        divTask.removeChild(Btndiv)
+        })
+
+        Btndiv.append(doneBtn)
+        Btndiv.append(delBtn)
+
+        divTask.append(Btndiv)
+        undoneDiv.insertBefore(divTask,undoneDiv.firstChild)
+
+        //Clear input box after Add
+        input.value = ''
     }
 })
-//show everything
-document.body.append(input)
-document.body.append(btn)
-document.body.append(mainDiv)
+
+//Reset Button
+const Resetbtn = document.querySelector('#Reset')
+Resetbtn.addEventListener('click', () => {
+    while (undoneDiv.firstChild) {
+        undoneDiv.removeChild(undoneDiv.lastChild);
+    }
+    while (doneDiv.firstChild) {
+        doneDiv.removeChild(doneDiv.lastChild);
+    }
+})
 
